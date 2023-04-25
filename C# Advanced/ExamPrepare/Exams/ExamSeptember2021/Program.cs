06060790aaed767ae155c1d46d804415896371e9
Radoslav Bogdanov <radimk8812@abv.bg>
2023-04-25 22:49:18 +0300

    C#

---
+++ C# Advanced/ExamPrepare/Exams/ExamSeptember2021/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MasterChef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dishesQue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            var freshnessStack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            //var cakeFreshness = 300;
+            //var lobsterFreshness = 400;
+            //var saladFreshness = 250;
+            //var sauceFreshness = 150;
+
+            var cakeCount = 0;
+            var sauceCount = 0;
+            var saladCount = 0;
+            var LobsterCount = 0;
+
+            while (true)
+            {
+                if (dishesQue.Count == 0)
+                {
+                    break;
+                }
+
+                if (freshnessStack.Count == 0)
+                {
+                    break;
+                }
+
+                var ingredient = dishesQue.Peek();
+                var freshness = freshnessStack.Peek();
+
+                if (ingredient == 0)
+                {
+                    dishesQue.Dequeue();
+                    continue;
+                }
+
+                if (ingredient * freshness == 150)
+                {
+                    sauceCount++;
+                    dishesQue.Dequeue();
+                    freshnessStack.Pop();
+                }
+                else if (ingredient * freshness == 250)
+                {
+                    saladCount++;
+                    dishesQue.Dequeue();
+                    freshnessStack.Pop();
+                }
+                else if (ingredient * freshness == 300)
+                {
+                    cakeCount++;
+                    dishesQue.Dequeue();
+                    freshnessStack.Pop();
+                }
+                else if (ingredient * freshness == 400)
+                {
+                    LobsterCount++;
+                    dishesQue.Dequeue();
+                    freshnessStack.Pop();
+                }
+                else
+                {
+                    freshnessStack.Pop();
+                    var increased = ingredient += 5;
+                    dishesQue.Dequeue();
+                    dishesQue.Enqueue(increased);
+                }
+            }
+
+            if (LobsterCount != 0 && saladCount != 0 && saladCount != 0 && cakeCount != 0)
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+                Console.WriteLine($"# Chocolate cake --> {cakeCount}");
+                Console.WriteLine($"# Dipping sauce --> {sauceCount}");
+                Console.WriteLine($"# Green salad --> {saladCount}");
+                Console.WriteLine($"# Lobster --> {LobsterCount}");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+        }
+    }
+}
+
---
+++ C# Advanced/ExamPrepare/Exams/TheBattleOfFiveArmies/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheBattleOfFiveArmies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armor = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+             
+            var matrix = new char[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentRow = Console.ReadLine().ToCharArray();
+                matrix[i] = currentRow;
+            }
+
+
+            var heroRow = 0;
+            var heroCol = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == 'A')
+                    {
+                        heroRow = i;
+                        heroCol = j;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                var commandLine = Console.ReadLine().Split(' ');
+                var command = commandLine[0];
+                var orcRow = int.Parse(commandLine[1]);
+                var orcCol = int.Parse(commandLine[2]);
+
+
+                matrix[orcRow][orcCol] = 'O';
+                matrix[heroRow][heroCol] = '-';
+
+                if (command == "up" && heroRow - 1 >= 0)
+                {
+                    heroRow--;
+                }
+                else if (command == "down" && heroRow + 1 < matrix[heroCol].Length)
+                {
+                    heroRow++;
+                }
+                else if (command == "left" && heroCol - 1 >= 0)
+                {
+                    heroCol--;
+                }
+                else if (command == "right" && heroCol + 1 < matrix[heroRow].Length)
+                {
+                    heroCol++;
+                }
+
+                if (matrix[heroRow][heroCol] == 'M')
+                {
+                    matrix[heroRow][heroCol] = '-';
+                    Console.WriteLine($"The army managed to free the Middle world! Armor left: {armor}");
+                    break;
+                }
+
+                if (matrix[heroRow][heroCol] == 'O')
+                {
+                    armor -= 2;
+                    matrix[heroRow][heroCol] = 'A';
+                }
+
+                if (armor <= 0)
+                {
+                    matrix[heroRow][heroCol] = 'X';
+                    Console.WriteLine($"The army was defeated at {heroRow};{heroCol}");
+                    break;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
