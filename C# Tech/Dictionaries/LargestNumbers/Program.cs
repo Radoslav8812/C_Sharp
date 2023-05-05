10d9749c0f92ad65a15090a0929d176596d856b6
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-05 17:07:39 +0300

    C#

---
+++ C# Tech/Dictionaries/LargestNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LargestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var times = 0;
+
+            if (input.Count >= 3)
+            {
+                foreach (var item in input.OrderByDescending(x => x))
+                {
+                    if (times == 3)
+                    {
+                        break;
+                    }
+
+                    Console.Write(item + " ");
+                    times++;
+                }
+            }
+            else
+            {
+                foreach (var item in input.OrderByDescending(x => x))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
+
