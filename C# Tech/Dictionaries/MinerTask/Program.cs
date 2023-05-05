17b150fdfc046764e3056070f0e52108a4df752c
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-05 17:33:15 +0300

    C#

---
+++ C# Tech/Dictionaries/MinerTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, int>();
+
+            while (true)
+            {
+                var resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+                var value = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(resource))
+                {
+                    dict.Add(resource, value);
+                }
+                else
+                {
+                    dict[resource] += value;
+                }
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
+
