56f64e06f0192e31e502c180b556d89fd8517ae9
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-05 17:13:57 +0300

    C#

--- C# Tech/Dictionaries/LargestNumbers/Program.cs
+++ C# Tech/Dictionaries/LargestNumbers/Program.cs
@@ -8,29 +8,32 @@ namespace LargestNumbers
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine().Split().Select(int.Parse).ToList();
-            var times = 0;
+            //var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //var times = 0;
 
-            if (input.Count >= 3)
-            {
-                foreach (var item in input.OrderByDescending(x => x))
-                {
-                    if (times == 3)
-                    {
-                        break;
-                    }
+            //if (input.Count >= 3)
+            //{
+            //    foreach (var item in input.OrderByDescending(x => x))
+            //    {
+            //        if (times == 3)
+            //        {
+            //            break;
+            //        }
 
-                    Console.Write(item + " ");
-                    times++;
-                }
-            }
-            else
-            {
-                foreach (var item in input.OrderByDescending(x => x))
-                {
-                    Console.Write(item + " ");
-                }
-            }
+            //        Console.Write(item + " ");
+            //        times++;
+            //    }
+            //}
+            //else
+            //{
+            //    foreach (var item in input.OrderByDescending(x => x))
+            //    {
+            //        Console.Write(item + " ");
+            //    }
+            //}
+
+
+            Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).Take(3).ToList().ForEach(Console.WriteLine);
         }
     }
 }
