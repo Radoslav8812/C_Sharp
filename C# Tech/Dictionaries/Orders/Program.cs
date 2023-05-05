a19a4c54148806cddda180d49c6fcaf8a05c926c
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-05 23:11:44 +0300

    C#

---
+++ C# Tech/Dictionaries/Orders/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var dict = new Dictionary<string, double[]>();
+
+            while (true)
+            {
+                if (input[0] == "buy")
+                {
+                    break;
+                }
+
+                var product = input[0];
+                var price = double.Parse(input[1]);
+                var quantity = int.Parse(input[2]);
+
+                if (!dict.ContainsKey(product))
+                {
+                    dict[product] = new double[] { price, quantity };
+                }
+                else
+                {
+                    double[] productInfo = dict[product];
+                    double existingPrice = productInfo[0];
+                    int existingQuantity = (int)productInfo[1];
+
+                    if (existingPrice != price)
+                    {
+                        productInfo[0] = price;
+                    }
+                    productInfo[1] = existingQuantity + quantity;
+                }
+
+
+                input = Console.ReadLine().Split();
+            }
+
+            foreach (KeyValuePair<string, double[]> product in dict)
+            {
+                string name = product.Key;
+                double price = product.Value[0];
+                int quantity = (int)product.Value[1];
+                double totalPrice = price * quantity;
+                Console.WriteLine($"{name} -> {totalPrice:F2}");
+            }
+        }
+    }
+}
+
