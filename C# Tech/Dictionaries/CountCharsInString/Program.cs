006b909621280836d11aab203a72862df862200c
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-05 17:21:47 +0300

    C#

---
+++ C# Tech/Dictionaries/CountCharsInString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountCharsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToCharArray();
+            var dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                {
+                    if (!dict.ContainsKey(text[i]))
+                    {
+                        dict.Add(text[i], 0);
+                    }
+
+                    dict[text[i]]++;
+                }
+            }
+
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
+
