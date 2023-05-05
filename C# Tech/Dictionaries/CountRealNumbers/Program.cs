31adf5fb4293765cc5da75fae410301b70ab6cc8
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-05 14:55:17 +0300

    C#

---
+++ C# Tech/Dictionaries/CountRealNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+            var dict = new Dictionary<double, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!dict.ContainsKey(input[i]))
+                {
+                    dict.Add(input[i], 1);
+                }
+                else
+                {
+                    dict[input[i]] += 1;
+                }
+            }
+
+            foreach (var item in dict.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
+
