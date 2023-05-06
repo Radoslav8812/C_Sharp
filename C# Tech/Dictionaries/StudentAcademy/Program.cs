5a5e9a2a0b57942ade94904aeaca571df81dbc9e
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-06 09:04:41 +0300

    C#

---
+++ C# Tech/Dictionaries/StudentAcademy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var name = Console.ReadLine();
+                var grade = double.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(name))
+                {
+                    dict.Add(name, new List<double>());
+                }
+
+                dict[name].Add(grade);
+            }
+
+            foreach (var kvp in dict.OrderByDescending(x => dict.Values).ThenByDescending(x => dict.Keys))
+            {
+                if (kvp.Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value.Average()}");
+                }
+                
+            }
+        }
+    }
+}
+
