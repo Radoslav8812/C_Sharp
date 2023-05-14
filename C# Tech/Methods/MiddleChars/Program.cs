acd706c7e570cbdda83c3859a24ea8c315718bdc
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-14 11:36:22 +0300

    C#

---
+++ C# Tech/Methods/MiddleChars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MiddleChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            PrintMiddleCharacter(input);
+        }
+
+        public static void PrintMiddleCharacter(string str)
+        {
+            int length = str.Length;
+
+            if (length % 2 != 0)
+            {
+                int middleIndex = length / 2;
+                Console.WriteLine(str[middleIndex].ToString());
+            }
+            else
+            {
+                int middleIndex = length / 2;
+                Console.WriteLine(str[middleIndex - 1].ToString() + str[middleIndex].ToString());
+            }
+        }
+    }
+}
+
