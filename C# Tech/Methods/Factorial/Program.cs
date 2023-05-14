29eab8465e09a6e22324a067af9e2bc39b2395ab
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-14 11:48:27 +0300

    C#

---
+++ C# Tech/Methods/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        long factorial1 = Factorial(num1);
+        int num2 = int.Parse(Console.ReadLine());
+        long factorial2 = Factorial(num2);
+
+        double divisionResult = (double)factorial1 / factorial2;
+
+        Console.WriteLine(divisionResult);
+    }
+
+    static long Factorial(int n)
+    {
+        long result = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+}
