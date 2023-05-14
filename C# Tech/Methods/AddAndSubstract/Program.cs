9dfcae323595da0c16d964410b46c8452f2abd70
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-14 11:22:58 +0300

    C#

---
+++ C# Tech/Methods/AddAndSubstract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AddAndSubstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            var num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(NewMethod(num1, num2, num3));
+        }
+
+        private static int NewMethod(int a, int b, int c)
+        {
+            var sum = a + b;
+            var result = (a + b) - c;
+
+            return result;
+        }
+    }
+}
+
