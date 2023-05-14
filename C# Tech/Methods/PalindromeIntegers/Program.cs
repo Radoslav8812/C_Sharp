e8666e139a65903140c3868f2b49f9bca92d79f1
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-14 12:17:33 +0300

    C#

---
+++ C# Tech/Methods/PalindromeIntegers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            input = NewMethod(input);
+        }
+
+        private static string NewMethod(string input)
+        {
+            while (true)
+            {
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (input.Length == 1)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    var first = input[0];
+                    var last = input[input.Length - 1];
+
+                    if (first == last)
+                    {
+                        Console.WriteLine("true");
+                    }
+                    else
+                    {
+                        Console.WriteLine("false");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            return input;
+        }
+    }
+}
+
