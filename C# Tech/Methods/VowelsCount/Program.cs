87fe43a9c1d1d850eae950792a637b94b55db172
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-14 09:33:47 +0300

    C#

---
+++ C# Tech/Methods/VowelsCount/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine().ToLower();
+            int count = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'u' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
