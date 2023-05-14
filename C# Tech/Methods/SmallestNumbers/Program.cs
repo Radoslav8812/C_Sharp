7df7470eeb1635f7a71bf5da83753910f74b5e84
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-14 09:46:25 +0300

    C#

---
+++ C# Tech/Methods/SmallestNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmallestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int smallest = num1;
+
+            if (num2 < smallest) 
+            {
+                smallest = num2;
+            }
+
+            if (num3 < smallest)
+            {
+                smallest = num3;
+            }
+
+            Console.WriteLine(smallest);
+        }
+    }
+    
+}
+
