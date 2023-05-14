2d37bfc050e22ff83831fc31a71cce512bc54d54
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-14 11:40:51 +0300

    C#

---
+++ C# Tech/Methods/NxNMatrix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            PrintMatrix(input);
+        }
+
+        public static void PrintMatrix(int n)
+        {
+            // iterate through the rows
+            for (int i = 0; i < n; i++)
+            {
+                // iterate through the columns
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+
+                Console.WriteLine(); // move to the next line after printing a row
+            }
+        }
+    }
+}
+
