7190cbfb1ff47e4413eb0e47125c946f01d2f12e
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-16 16:11:56 +0300

    C#

---
+++ C# Tech/Text Procesing/Valid Usernames/ValidUsernames/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ").ToArray();
+            var list = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var word = input[i];
+
+                if (word.Length >= 3 && word.Length <= 16 && Regex.IsMatch(word ,@"^[a-zA-Z0-9_-]+$"))
+                {
+                    list.Add(word);
+                }
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
