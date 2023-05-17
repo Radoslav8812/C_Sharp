b1b01286dfd44df7ba66267ad25a766415e2bdbb
Radoslav Bogdanov <radimk8812@abv.bg>
2023-05-17 12:55:41 +0300

    C#

---
+++ C# Tech/Text Procesing/ExtractFiles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ExtractFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split("\\");
+            var currentItem = input[input.Length - 1];
+            var splitedItem = currentItem.Split(".");
+
+            Console.WriteLine($"File name: {splitedItem[0]}");
+            Console.WriteLine($"File extension: {splitedItem[1]}");
+        }
+    }
+}
+
